bc0a8e6ffa1209da16778679ada2a4ab5b0a8af0
ravinder <ravinder.nitw@gmail.com>
2016-03-10 15:49:11 +0530

    after add operation for counting zeros code

--- src/com/algo/general/CountZeros.java
+++ src/com/algo/general/CountZeros.java
@@ -2,10 +2,30 @@ package com.algo.general;
 
 public class CountZeros {
 
- 
 	public static void main(String[] args) {
-	
-		int[] in = {0,0,0,0,0,0,0,0,0,0,0,0,2,5,7,10};
+
+		int[] in = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 5, 7, 10 };
+		int len = in.length;
+		int result = countZeros(in, len);
+		System.out.println("Number of zeros in array " + result);
+	}
+
+	private static int countZeros(int[] in, int len) {
+		int low = 0;
+		int high = len - 1;
+		int mid;
+		while (low <= high) {
+			mid = low + (high - low) / 2;
+			if (in[mid] == 0)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		if (in[high] == 0)
+			return high + 1;
+		if (in[low] == 0)
+			return low + 1;
+		return -1;
 	}
 
 }
